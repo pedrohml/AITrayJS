43b1b46a3fd5ff85cd1a2706818b27b439b18723
Pedro Lira <pedrohml@gmail.com>
2023-02-04 21:59:12 -0300

    Add package makers for zip and squirrel

--- forge.config.js
+++ forge.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   packagerConfig: {},
   makers: [
-      // {
-      //   "name": "@electron-forge/maker-squirrel",
-      //   "config": {
-      //     "name": "electron_quick_start"
-      //   }
-      // },
-      // {
-      //   "name": "@electron-forge/maker-zip",
-      //   "platforms": [
-      //     "darwin"
-      //   ]
-      // },
+      {
+        "name": "@electron-forge/maker-squirrel",
+        "config": {
+          "name": "electron_quick_start"
+        }
+      },
+      {
+        "name": "@electron-forge/maker-zip",
+        "platforms": [
+          "darwin", "linux", "win32"
+        ]
+      },
       {
         "name": "@electron-forge/maker-deb",
         "config": {}
